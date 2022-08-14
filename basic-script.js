a4b581baaba07673e2cf4ae6ed8b514786f35ec3
Nur Doğan <96629589+NurDogan7@users.noreply.github.com>
2022-08-15 02:08:07 +0300

    Form-exercise

---
+++ basic-script.js
@@ -0,0 +1,113 @@
+// DEĞİŞKENLER
+// var maasAli = 5000;
+// var maasCan = 6000;
+
+// var zam = 0.3;
+
+// console.log(maasAli + (maasAli * zam))
+// console.log(maasCan + (maasCan * zam))
+
+// DEĞİŞKEN TÜRLERİ
+
+// var urunAdi = "iphone 13"; // string
+// let urunFiyat = 15000; // number
+
+// console.log(typeof urunAdi);
+// console.log(typeof urunFiyat);
+
+// let sayi1 = 10;
+// let sayi2 = 20;
+
+// console.log(sayi1 + sayi2);
+// // src = 30
+
+// let sayi3 = "10";
+// let sayi4 = "20";
+
+// console.log(Number(sayi3) + Number(sayi4));
+// // src = 30
+
+// let sayi5 = 10;
+// let sayi6 = 20;
+
+// console.log(sayi5.toString() + sayi6.toString());
+// // src = 1020
+
+// let ad = "Havvanur";
+// let soyad = "Doğan";
+
+// console.log(ad + " " + soyad);
+// src = Havvanur Doğan
+
+// let sinavNotu = 70
+// let basarili = (sinavNotu >= 50);
+// let basarisiz = (sinavNotu < 50);
+
+// console.log(basarili);
+// src = true
+
+// let yas;
+
+// console.log(typeof yas);
+// src = undifined
+
+// DEĞİŞKENLER-2
+// let ogrenci1 = {
+//     isim: "Havvanur",
+//     dogumtarihi: "15.07.2000",
+//     not: 80
+// };
+
+// let ogrenci2 = {
+//     isim: "Ülkü",
+//     dogumtarihi: "22.08.2000",
+//     not: 50
+// };
+
+
+// console.log(ogrenci1);
+// console.log(ogrenci2);
+
+// ARİTMETİK OPERATÖRLER
+// let a= 10;
+// let b= 2;
+// let sonuc;
+
+// // sonuc = a + b;
+// // sonuc = a - b;
+// // sonuc = a * b;
+// // sonuc = a  / b;
+// // sonuc = ++a;
+// // sonuc = --a;
+
+// // // ATAMA OPERATÖRLERİ
+// // sonuc = a;
+// // sonuc += a;
+// // sonuc -= a;
+// // sonuc *= a;
+// // sonuc /= a;
+// // sonuc %= a;
+
+// // KARŞILAŞTIRMA OPERATÖRLERİ
+// sonuc = (a == b)
+// sonuc = (a != b)
+// sonuc = (a === "3")
+// sonuc = (a > b)
+// console.log(sonuc);
+
+// IF-ELSE KOŞULU
+
+let a = 5;
+let b = 10;
+
+if (a > b) [
+    console.log("a, b'den büyük")
+]
+
+else if (a == b) {
+    console.log("a eşittir b'ye")
+}
+
+else {
+    console.log("a b'den küçük")
+} 
